d5d36e0601efd2e11901c3fd699b3d17e6a63975
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-08-25 19:15:54 +0800

    refactor: modified bot name and hello string

---
+++ src/main/java/FloydAI.java
@@ -0,0 +1,11 @@
+public class FloydAI {
+    public static void main(String[] args) {
+        String hello = "____________________________________________________________\n"
+                + " Hello! I'm FloydAI\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(hello);
+    }
+}
