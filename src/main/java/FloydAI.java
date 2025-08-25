80c2ed8ef22d2ef4096e438d74dc4cfc2d33cebc
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-08-25 19:31:15 +0800

    feat: added Task class and ability to mark tasks as done

--- src/main/java/FloydAI.java
+++ src/main/java/FloydAI.java
@@ -1,48 +1,66 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FloydAI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100]; // store tasks
+        ArrayList<Task> tasks = new ArrayList<>();
         int taskCount = 0;
 
         // Greet the user
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm George Floyd");
+        System.out.println(" Hello! I'm FloydAI");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
         while (true) {
-            String input = sc.nextLine(); // read user input
+            String input = sc.nextLine().trim();
 
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
-                break; // exit loop
+                break;
             } else if (input.equals("list")) {
-                // Show all tasks
                 System.out.println("____________________________________________________________");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + "." + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    Task t = tasks.get(index);
+                    t.markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + t);
+                    System.out.println("____________________________________________________________");
+                } catch (Exception e) {
+                    System.out.println("Invalid task number!");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    Task t = tasks.get(index);
+                    t.markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + t);
+                    System.out.println("____________________________________________________________");
+                } catch (Exception e) {
+                    System.out.println("Invalid task number!");
+                }
             } else {
                 // Add task
-                if (taskCount < tasks.length) {
-                    tasks[taskCount] = input;
-                    taskCount++;
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" added: " + input);
-                    System.out.println("____________________________________________________________");
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" Task list is full!");
-                    System.out.println("____________________________________________________________");
-                }
+                Task t = new Task(input);
+                tasks.add(t);
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
         }
-
         sc.close();
     }
 }
