1a6c91a6ec76daec481bc05a4cbf2bb88e45896e
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-08-25 19:24:48 +0800

    feat: Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/FloydAI.java
+++ src/main/java/FloydAI.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class FloydAI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100]; // store tasks
+        int taskCount = 0;
 
         // Greet the user
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm FloydAI");
+        System.out.println(" Hello! I'm George Floyd");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
@@ -18,12 +20,27 @@ public class FloydAI {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break; // exit loop
+            } else if (input.equals("list")) {
+                // Show all tasks
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                // Add task
+                if (taskCount < tasks.length) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" added: " + input);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Task list is full!");
+                    System.out.println("____________________________________________________________");
+                }
             }
-
-            // Echo the input
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         sc.close();
